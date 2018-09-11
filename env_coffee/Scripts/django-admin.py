2a574c5b7ac715fe903bcea3ee37681352242611
ljpalaciom <ljpalaciom@eafit.edu.co>
2018-09-11 18:49:10 -0500

    Creando base del proyecto

---
+++ env_coffee/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\documents\pythonprojects\roya_cafe\env_coffee\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
