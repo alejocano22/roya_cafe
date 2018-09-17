4d3d498f6f0ecea5ccf564fe3318e7e8c329ed04
Luis Javier <ljpalaciom@eafit.edu.co>
2018-09-17 17:22:54 -0500

    Configurando contexto como objetos json para pintar adecuadamente el mapa

--- coffee_rescuer/apps/finca/views.py
+++ coffee_rescuer/apps/finca/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from apps.finca.models import Finca
 from apps.lote.models import Lote
 import json
+from django.core import serializers
 #import os
 # Create your views here.
 
@@ -12,15 +13,15 @@ def mapa_view(request,id_finca):
 	finca = Finca.objects.get(id = id_finca)
 	lotes = Lote.objects.filter(finca = id_finca)
 	coordenadas = finca.obtener_coordenadas(str(id_finca))
-	detalles_lote = {}
+	etapas = {}
 	for lote in lotes:
 		detalle_lote_actual = lote.obtener_detalle_lote_actual()
 		if detalle_lote_actual:
-			detalles_lote[lote.id]= detalle_lote_actual
+			etapas[lote.id]= detalle_lote_actual.etapa_hongo
 		else:
-			detalles_lote[lote.id]=lote.ultimo_estado_hongo
+			etapas[lote.id]=lote.ultimo_estado_hongo
 			
-	context = {"finca": finca, "lotes":lotes, "detalles_lote": detalles_lote,"coordenadas":json.dumps(coordenadas)}
+	context = {"finca": finca, "lotes":serializers.serialize('json',lotes,fields=["id","nombre"]), "etapas": json.dumps(etapas),"coordenadas":json.dumps(coordenadas)}
 	return render(request,"finca/mapa.html",context)
 
 
