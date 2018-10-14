e463504ff86fe7f3241770d128b5f07463d57f86
Sebastián Giraldo Gómez <sebastian.giraldo@cuemby.com>
2018-10-14 13:41:30 -0500

    Se agrega editor de mapas

--- coffee_rescuer/apps/finca/urls.py
+++ coffee_rescuer/apps/finca/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.finca.views import mapa_view
+from apps.finca.views import mapa_view, editor_view
 
 app_name = "finca"
 urlpatterns = [
-    path('<int:id_finca>/',mapa_view,name="mapa"), 
+    path('<int:id_finca>/',mapa_view,name="mapa"),
+    path('editor/<int:id_finca>/',editor_view,name="editor"),
 ]
