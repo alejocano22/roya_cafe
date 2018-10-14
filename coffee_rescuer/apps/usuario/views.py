12b7532bb6a633abff7ea8d8e376ba84f06561ee
Luis Javier <ljpalaciom@eafit.edu.co>
2018-10-14 11:33:55 -0500

    Ahora se actualiza el detalle de lote del usuario cuando este ingresa a la pagina

--- coffee_rescuer/apps/usuario/views.py
+++ coffee_rescuer/apps/usuario/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from apps.finca.models import Finca
 from django.contrib.auth.decorators import login_required
-from django.core import serializers
+from apps.lote.tasks import actualizar_info_usuario
 
 # Create your views here.
 @login_required
@@ -12,6 +12,7 @@ def fincas_listar(request):
     """
     if request.user.is_staff:
         return redirect('admin:index')
+    actualizar_info_usuario(request.user.username)
     fincas = Finca.objects.filter(usuario=request.user.id)
     contexto = {"fincas": fincas}
     return render(request, "usuario/fincas.html", contexto)
