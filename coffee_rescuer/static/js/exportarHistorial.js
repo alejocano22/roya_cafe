d3df39f25950ce0199122af953650ae3c5d04a20
Luis Javier <ljpalaciom@eafit.edu.co>
2018-10-14 19:43:15 -0500

    Modificando extension de archivo exportado en excel

--- coffee_rescuer/static/js/exportarHistorial.js
+++ coffee_rescuer/static/js/exportarHistorial.js
@@ -1,5 +1,5 @@
 var wb = XLSX.utils.table_to_book(document.getElementById('mytable'), {sheet:"Sheet JS"});
-var wbout = XLSX.write(wb, {bookType:'csv', bookSST:true, type: 'binary'});
+var wbout = XLSX.write(wb, {bookType:'xlsx', bookSST:true, type: 'binary'});
 function s2ab(s) {
     var buf = new ArrayBuffer(s.length);
     var view = new Uint8Array(buf);
@@ -7,5 +7,5 @@ function s2ab(s) {
     return buf;
 }
 $("#button-a").click(function(){
-    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'HistorialDatos.csv');
+    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'HistorialDatos.xlsx');
 });
