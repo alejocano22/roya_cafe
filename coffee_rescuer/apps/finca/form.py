e27b461aff05181547492e0fb23358304e236db2
Luis Javier Palacio Mesa <ljpalaciom@eafit.edu.co>
2018-10-18 09:10:17 -0500

    Update form.py

--- coffee_rescuer/apps/finca/form.py
+++ coffee_rescuer/apps/finca/form.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms.widgets import HiddenInput
-from apps.finca.models import Finca
+
 
 class EditorForm(forms.Form):
     jsonfield = forms.CharField(widget=HiddenInput(attrs={'id': 'infoNuevoMapa'}))
